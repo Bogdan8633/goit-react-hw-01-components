a4a655c748fc0e3d9861cd5ba1029d4561856da6
Bogdan <bogdanberezka@gmail.com>
2023-01-14 16:56:21 +0200

    first-task-done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,38 @@
+import Profile from './Profile/Profile';
+import user from '../data/task1/user.json';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        // stats=""
+        followers={user.stats.followers}
+        views={user.stats.views}
+        likes={user.stats.likes}
+      ></Profile>
+    </>
+
+    // username — имя пользователя
+    // tag — тег в социальной сети без @
+    // location — город и страна
+    // avatar — ссылка на изображение
+    // stats — объект с информацией об активности
+
+    // <div
+    //   style={{
+    //     height: '100vh',
+    //     display: 'flex',
+    //     justifyContent: 'center',
+    //     alignItems: 'center',
+    //     fontSize: 40,
+    //     color: '#010101',
+    //   }}
+    // >
+    //   React homework template
+    // </div>
   );
 };
