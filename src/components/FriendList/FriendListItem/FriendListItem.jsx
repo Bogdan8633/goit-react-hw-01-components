bea169e496879e53ae64b11a3947047b74e0e19f
Bogdan <bogdanberezka@gmail.com>
2023-01-15 12:42:19 +0200

    make-randor-colorizer

--- src/components/FriendList/FriendListItem/FriendListItem.jsx
+++ src/components/FriendList/FriendListItem/FriendListItem.jsx
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import styles from './friend-list-item.module.css';
 
 const FriendListItem = ({ avatar, name, isOnline }) => {
+  // const randomColor = Math.floor(Math.random() * 16777215).toString(16);
   return (
     <li className={styles.item}>
       <span
--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -3,12 +3,19 @@ import PropTypes from 'prop-types';
 import styles from './statistics.module.css';
 
 const Statistics = ({ title, stats }) => {
-  const elements = stats.map(({ id, label, percentage }) => (
-    <li key={id} className={styles.item}>
-      <span className={styles.label}>{label}</span>
-      <span className={styles.percentage}>{percentage}%</span>
-    </li>
-  ));
+  const elements = stats.map(({ id, label, percentage }) => {
+    const randomColor = Math.floor(Math.random() * 16777215).toString(16);
+    return (
+      <li
+        key={id}
+        style={{ backgroundColor: `#${randomColor}` }}
+        className={styles.item}
+      >
+        <span className={styles.label}>{label}</span>
+        <span className={styles.percentage}>{percentage}%</span>
+      </li>
+    );
+  });
   return (
     <section className={styles.statistics}>
       {title && <h2 className={styles.title}>{title}</h2>}
